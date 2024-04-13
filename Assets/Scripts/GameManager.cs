c365405e86c67d6cff6455897cdfede9e9db425d
Pascal De Lisio <pdelisio@donetechno.com>
2024-04-13 12:33:21 -0400

    Reload scene when pressing "R"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using Dreamlings.Explorations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,6 +22,14 @@ public class GameManager : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
     public void GameOver()
     {
         Debug.Log("Game Over!");
@@ -28,5 +37,11 @@ public class GameManager : MonoBehaviour
         {
             gameOverManager.DisplayGameOver(2461);
         }
+    }
+
+    private void ReloadCurrentScene()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
     }
 }
