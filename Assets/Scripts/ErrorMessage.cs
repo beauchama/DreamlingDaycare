c72e37a5fd829530b974f75457cd9c8beae7c971
Pascal De Lisio <pdelisio@donetechno.com>
2024-04-14 12:33:49 -0400

    Fade error message

--- Assets/Scripts/ErrorMessage.cs
+++ Assets/Scripts/ErrorMessage.cs
@@ -1,22 +1,37 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class ErrorMessage : MonoBehaviour
 {
     public TextMeshProUGUI errorText;
+    public CanvasGroup canvasGroup;
+
+    private bool isDisplaying;
 
     public void DisplayError(string message)
     {
+        if (isDisplaying)
+            return;
+
         CancelInvoke(nameof(HideError));
 
         errorText.text = message;
+        canvasGroup.alpha = 0;
         gameObject.SetActive(true);
+        canvasGroup.DOFade(1, 0.5f).WaitForCompletion();
+        isDisplaying = true;
 
         Invoke(nameof(HideError), 3f);
     }
 
     public void HideError()
     {
-        gameObject.SetActive(false);
+        canvasGroup.DOFade(0, 0.5f).OnComplete(
+            () =>
+            {
+                isDisplaying = false;
+                gameObject.SetActive(false);
+            });
     }
 }
